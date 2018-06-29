1ee746236be61ef979b01b809108acd35252bcf8
faz <aminf3@coventry.ac.uk>
2018-06-30 06:10:21 +0800

    changed frontend folder name

--- backend/app.js
+++ backend/app.js
@@ -24,10 +24,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../game-showcase-front')));
+
+
 app.use(session({
   secret: 'SESSION_SECRET', 
-  resave: true, 
+  resave: false, 
   saveUninitialized: true,
 }))
 
